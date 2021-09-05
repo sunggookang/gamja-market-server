8f74b3d42f1c5f7a94084dd2765f668a933caeb2
sunggookang <rkdtjdrn90@gmail.com>
2021-09-05 14:48:44 +0900

    감자 마켓 서버 시작(복사본)

--- index.js
+++ index.js
@@ -1,127 +1,138 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 8080;
-const models = require('./models');
+const port = 8080;
 const multer = require('multer');
-const upload = multer({
-    storage: multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, 'uploads/')
-        },
-        filename: function (req, file, cb) {
-            cb(null, file.originalname)
-        }
-    })
+// const upload = multer({
+//     storage: multer.diskStorage({
+//         destination: function (req, file, cb) {
+//             cb(null, 'uploads/')
+//         },
+//         filename: function (req, file, cb) {
+//             cb(null, file.originalname)
+//         }
+//     })
+// });
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '111111',
+    database: 'gamjamarket'
 });
+db.connect();
 
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static('uploads'));
+// app.use('/uploads', express.static('uploads'));
 
-app.get('/banners', (req, res) => {
-    models.Banner.findAll({
-        limit: 2
-    }).then((result) => {
-        res.send({
-            banners: result
-        })
-    }).catch((error) => {
-        console.error(error);
-        res.status(500).send('에러가 발생했습니다.');
-    })
-});
+// app.get('/banners', (req, res) => {
+//     models.Banner.findAll({
+//         limit: 2
+//     }).then((result) => {
+//         res.send({
+//             banners: result
+//         })
+//     }).catch((error) => {
+//         console.error(error);
+//         res.status(500).send('에러가 발생했습니다.');
+//     })
+// });
 
 app.get("/products", (req, res) => {
-    models.Product.findAll({
-        order: [['createdAt', 'DESC']],
-        attributes: ['id', 'name', 'price', 'createdAt', 'seller', 'imageUrl', 'soldout']
-    }).then((result) => {
-        console.log("PRODUCTS : ", result);
+    // models.Product.findAll({
+    //     order: [['createdAt', 'DESC']],
+    //     attributes: ['id', 'name', 'price', 'createdAt', 'seller', 'imageUrl', 'soldout']
+    // }).then((result) => {
+    //     console.log("PRODUCTS : ", result);
+    //     res.send({
+    //         products: result
+    //     })
+    // }).catch((error) => {
+    //     console.error(error);
+    //     res.status(400).send("에러 발생");
+    // })
+    db.query(`SELECT * FROM products`, (error, result) => {
         res.send({
             products: result
         })
-    }).catch((error) => {
-        console.error(error);
-        res.status(400).send("에러 발생");
     })
 })
 
-app.post("/products", (req, res) => {
-    const body = req.body;
-    const { name, description, price, seller, imageUrl } = body;
-    if (!name || !description || !price || !seller || !imageUrl) {
-        res.status(400).send("모든 필드를 입력해주세요.");
-    }
-    models.Product.create({
-        name: name,
-        description: description,
-        price: price,
-        seller: seller,
-        imageUrl: imageUrl
-    }).then((result) => {
-        console.log("상품 생성 결과 : ", result);
-        res.send({
-            result: result
-        })
-    }).catch((error) => {
-        console.error(error);
-        res.status(400).send("상품 업로드에 문제가 발생했습니다.");
-    })
-})
+// app.post("/products", (req, res) => {
+//     const body = req.body;
+//     const { name, description, price, seller, imageUrl } = body;
+//     if (!name || !description || !price || !seller || !imageUrl) {
+//         res.status(400).send("모든 필드를 입력해주세요.");
+//     }
+//     models.Product.create({
+//         name: name,
+//         description: description,
+//         price: price,
+//         seller: seller,
+//         imageUrl: imageUrl
+//     }).then((result) => {
+//         console.log("상품 생성 결과 : ", result);
+//         res.send({
+//             result: result
+//         })
+//     }).catch((error) => {
+//         console.error(error);
+//         res.status(400).send("상품 업로드에 문제가 발생했습니다.");
+//     })
+// })
 
-app.get("/products/:id", (req, res) => {
-    const params = req.params;
-    const { id } = params;
-    models.Product.findOne({
-        where: {
-            id: id
-        }
-    }).then((result) => {
-        console.log("PRODUCT : ", result);
-        res.send({
-            product: result
-        })
-    }).catch((error) => {
-        console.error(error);
-        res.status(400).send("상품 조회 에러가 발생했습니다.");
-    })
-})
+// app.get("/products/:id", (req, res) => {
+//     const params = req.params;
+//     const { id } = params;
+//     models.Product.findOne({
+//         where: {
+//             id: id
+//         }
+//     }).then((result) => {
+//         console.log("PRODUCT : ", result);
+//         res.send({
+//             product: result
+//         })
+//     }).catch((error) => {
+//         console.error(error);
+//         res.status(400).send("상품 조회 에러가 발생했습니다.");
+//     })
+// })
 
-app.post('/image', upload.single('image'), (req, res) => {
-    const file = req.file;
-    console.log(file);
-    res.send({
-        imageUrl: file.path
-    })
-});
+// app.post('/image', upload.single('image'), (req, res) => {
+//     const file = req.file;
+//     console.log(file);
+//     res.send({
+//         imageUrl: file.path
+//     })
+// });
 
-app.post('/purchase/:id', (req, res) => {
-    const { id } = req.params;
-    models.Product.update({
-        soldout: 1
-    }, {
-        where: {
-            id
-        }
-    }).then((result) => {
-        res.send({
-            result: true
-        })
-    }).catch((error) => {
-        console.error(error);
-        res.status(500).send("에러가 발생했습니다.");
-    })
-});
+// app.post('/purchase/:id', (req, res) => {
+//     const { id } = req.params;
+//     models.Product.update({
+//         soldout: 1
+//     }, {
+//         where: {
+//             id
+//         }
+//     }).then((result) => {
+//         res.send({
+//             result: true
+//         })
+//     }).catch((error) => {
+//         console.error(error);
+//         res.status(500).send("에러가 발생했습니다.");
+//     })
+// });
 
 app.listen(port, () => {
     console.log("감자의 쇼핑몰 서버가 돌아가고 있습니다.");
-    models.sequelize.sync().then(() => {
-        console.log('DB 연결 성공!');
-    }).catch((err) => {
-        console.error(err);
-        console.log('DB 연결 에러!');
-        process.exit();
-    })
-
+    // models.sequelize.sync().then(() => {
+    //     console.log('DB 연결 성공!');
+    // }).catch((err) => {
+    //     console.error(err);
+    //     console.log('DB 연결 에러!');
+    //     process.exit();
+    // })
 })
